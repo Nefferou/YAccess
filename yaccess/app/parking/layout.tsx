1e79dd8625224eacd4811e7da98c54aca522c8ca
Andréas CASTELLO <andreas.castello06@gmail.com>
2023-11-24 15:35:40 +0100

    Update layout

--- yaccess/app/parking/layout.tsx
+++ yaccess/app/parking/layout.tsx
@@ -1,7 +1,8 @@
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
+import React from "react";
 
-export default function Layout({ children }) {
+export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <>
             <Header />

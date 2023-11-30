8adc22723caa5b63a3a5d15ce4debaefbf9e8614
Carlos Vargas <varlopecar@gmail.com>
2023-11-30 16:28:05 +0100

    fix: bug page.tsx

--- yaccess/app/page.tsx
+++ yaccess/app/page.tsx
@@ -9,11 +9,10 @@ export default async function Home() {
 
   return (
     <main className="h-screen px-8 pt-20 flex flex-col items-center gap-8">
-        <h1 className="text-3xl font-semibold tracking-wider">Home</h1>
-        <div className="flex flex-col gap-10">
-            <ChoiceCard title="Carpooling" svg={carpooling} onLeftImage={false} link="/carpooling"></ChoiceCard>
-            <ChoiceCard title="Parking" svg={parking} onLeftImage={true} link="/parking"></ChoiceCard>
-        </div>
+      <h1 className="text-3xl font-semibold tracking-wider">Home</h1>
+      <div className="flex flex-col gap-10">
+        <ChoiceCard title="Carpooling" svg={carpooling} onLeftImage={false} link="/carpooling"></ChoiceCard>
+        <ChoiceCard title="Parking" svg={parking} onLeftImage={true} link="/parking"></ChoiceCard>
       </div>
     </main>
   )

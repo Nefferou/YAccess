2c6b6e6eb983b6c5f3195f3c3f251804fce261bb
EnzoCasalini <enzocasa83@gmail.com>
2023-11-30 17:13:43 +0100

    Title component

---
+++ yaccess/components/ui/Title/Title.tsx
@@ -0,0 +1,23 @@
+type TitleProps = {
+    text: string;
+    subtitle?: string;
+    placement?: 'left' | 'center' | 'right';
+}
+
+const Title = ({ text, subtitle, placement = 'left' }: TitleProps) => {
+    const placementClass = {
+        'left': 'text-left',
+        'center': 'text-center',
+        'right': 'text-right',
+    }[placement];
+
+    return (
+        <>
+            <h1 className={`text-3xl font-semibold ${placementClass} tracking-wider`}>{text}</h1>
+            {subtitle && <h2 className={`text-xs font-bold ${placementClass}`}>{subtitle}</h2>}
+        </>
+    );
+};
+
+
+export default Title;

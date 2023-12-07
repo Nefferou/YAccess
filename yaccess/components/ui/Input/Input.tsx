16d62c7875b54e27f285c7c371baf88dc2e73ea0
Andréas CASTELLO <andreas.castello06@gmail.com>
2023-12-07 14:54:09 +0100

    Add Input Component and move Popup dir

---
+++ yaccess/components/ui/Input/Input.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Image from 'next/image';
+/*
+UTILISATION
+
+import Input from "../components/ui/Input/Input";
+
+<Input
+    icon={carpooling}
+    size="normal"
+    placeholder="Enter your username"
+/>
+<Input
+    icon={parking}
+    size="small"
+    placeholder="Search..."
+/>
+ */
+
+const Input = ({ icon, size, placeholder }) => {
+    const widthClass = size === 'small' ? 'w-1/2' : 'w-full';
+    return (
+        <div className={`flex items-center rounded h-12 ${widthClass} p-2 bg-white-primary`}>
+            {icon && (
+                <div className="mr-2 flex-shrink-0">
+                    <Image src={icon} alt="" width={20} height={20} />
+                </div>
+            )}
+            <input
+                type="text"
+                placeholder={placeholder}
+                className="flex-1 bg-transparent outline-none text-black"
+            />
+        </div>
+    );
+};
+
+Input.propTypes = {
+    icon: PropTypes.object,
+    size: PropTypes.oneOf(['small', 'medium']),
+    placeholder: PropTypes.string,
+};
+
+export default Input;

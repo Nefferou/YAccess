7ffb2fa9dd5c508c49df722478b9c20f4de7e3d7
Nefferou <fertijulien@gmail.com>
2023-12-07 17:03:12 +0100

    UserContext / Add Provider to all layouts

--- yaccess/app/carpooling/layout.tsx
+++ yaccess/app/carpooling/layout.tsx
@@ -1,12 +1,17 @@
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import React from "react";
+import { UserContextProvider } from "../../contexts/userContext";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <>
             <Header />
-            <main>{children}</main>
+            <main>
+                <UserContextProvider>
+                    {children}
+                </UserContextProvider>
+            </main>
             <Footer />
         </>
     )
